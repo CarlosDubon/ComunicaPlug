59d0d378483f02374e24b63a504a5b2bb4ca6a4a
CarlosDubon <00058016@uca.edu.sv>
2018-03-01 13:30:46 -0600

    asi?

--- includes/widget/widget.php
+++ includes/widget/widget.php
@@ -12,12 +12,14 @@ class radio extends WP_Widget {
         // Contenido del Widget que se mostrará en la Sidebar
         echo $before_widget;
         ?>
-        <div class="card text-white bg-primary mb-3">
-          <div class="card-header">Header</div>
-          <div class="card-body">
-            <h4 class="card-title">Primary card title</h4>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          </div>
+        <div class="col-md-6">
+            <div class="card text-white bg-primary mb-3">
+              <div class="card-header">Header</div>
+              <div class="card-body">
+                <h4 class="card-title">Primary card title</h4>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+            </div>
         </div>
         <?php
         echo $after_widget;
