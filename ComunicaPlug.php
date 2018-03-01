2e3932243fb80fd62574e2f89dbb5495359a9283
CarlosDubon <00058016@uca.edu.sv>
2018-03-01 13:05:47 -0600

    ahora

--- ComunicaPlug.php
+++ ComunicaPlug.php
@@ -6,8 +6,8 @@
 * Author: Comunica  
 */
 
-function mpw_create_widget(){
+function ComunicaPlug(){
     include_once(plugin_dir_path( __FILE__ ).'/includes/widget/widget.php');
     register_widget('radio');
 }
-add_action('widgets_init','radio');
+add_action('widgets_init','ComunicaPlug');
