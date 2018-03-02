1fcc00577bfb7acce90248471063635b5024adff
CarlosDubon <00058016@uca.edu.sv>
2018-03-02 14:05:15 -0600

    prueba si funciona

---
+++ includes/widget/FSlider.php
@@ -0,0 +1,26 @@
+<?php
+
+class FSlider extends WP_Widget {
+
+    function ISlider(){
+        // Constructor del Widget.
+        $widget_ops = array('classname'=>'FSlider', 'description'=>'Cierra un slider');
+        $this->WP_Widget('FSlider','Cierre de Slider <slider>',$widget_ops);
+    }
+
+    function widget($args,$instance){
+        // Contenido del Widget que se mostrará en la Sidebar
+        echo '</div>';
+    }
+
+    function update($new_instance, $old_instance){
+        // Función de guardado de opciones
+       
+    }
+
+    function form($instance){
+       
+    }
+}
+
+?>
