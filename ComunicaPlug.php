69a84c779c86017cd56edfa124a77aa8d7972a37
CarlosDubon <00058016@uca.edu.sv>
2018-03-02 14:14:39 -0600

    ?

--- ComunicaPlug.php
+++ ComunicaPlug.php
@@ -7,9 +7,9 @@
 */
 
 function ComunicaPlug(){
-    require(plugin_dir_path( __FILE__ ).'/includes/widget/ISlider.php');
+    include_once(plugin_dir_path( __FILE__ ).'/includes/widget/ISlider.php');
     register_widget('ISlider');
-    require(plugin_dir_path( __FILE__ ).'/includes/widget/FSlider.php');
-    register_widget('FSlider');
+    //include_once(plugin_dir_path( __FILE__ ).'/includes/widget/FSlider.php');
+    //register_widget('FSlider');
 }
 add_action('widgets_init','ComunicaPlug');
