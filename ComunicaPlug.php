6ef3a504fbb8e0d386837864fdf1d03b5b750048
CarlosDubon <00058016@uca.edu.sv>
2018-04-08 21:13:20 -0600

    dsa

--- ComunicaPlug.php
+++ ComunicaPlug.php
@@ -11,5 +11,7 @@ function ComunicaPlug(){
     register_widget('ISlider');
     require plugin_dir_path( __FILE__ ).'/includes/widget/FSlider.php';
     register_widget('FSlider');
+    require plugin_dir_path( __FILE__ ).'/includes/widget/Podcast.php';
+    register_widget('Podcast');
 }
 add_action('widgets_init','ComunicaPlug');
