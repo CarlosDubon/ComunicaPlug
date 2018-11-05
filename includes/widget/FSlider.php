3fddc253df90cdef29ae2093d48a3a8f9d60d8d2
CarlosDubon <00058016@uca.edu.sv>
2018-11-05 15:06:17 -0600

    fix

--- includes/widget/FSlider.php
+++ includes/widget/FSlider.php
@@ -5,7 +5,7 @@ class FSlider extends WP_Widget {
     function __construct(){
         // Constructor del Widget.
         $widget_ops = array('classname'=>'FSlider', 'description'=>'Cierra un slider');
-        $this->WP_Widget('FSlider','Cierre de Slider <slider>',$widget_ops);
+        $this->parent::__construct('FSlider','Cierre de Slider <slider>',$widget_ops);
     }
 
     function widget($args,$instance){
